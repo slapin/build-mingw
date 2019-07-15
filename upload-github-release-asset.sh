6bb46aa93473daab6d383f7e2571e76caef49a1b
Segey Lapin <slapin@ossfans.org>
2019-07-15 04:04:04 +0300

    Fixed response ID

--- upload-github-release-asset.sh
+++ upload-github-release-asset.sh
@@ -51,10 +51,11 @@ curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or 
 # Read asset tags.
 response=$(curl -sH "$AUTH" $GH_TAGS)
 
-curl "$GITHUB_OAUTH_BASIC" -H "Authorization: token $github_api_token" --data "{ \"tag_name\": \"$tag\", \"name\": \"$repo $tag\", \"body\": \"\", \"draft\": true }" $GH_REPO/releases
+response=$(curl "$GITHUB_OAUTH_BASIC" -H "Authorization: token $github_api_token" --data "{ \"tag_name\": \"$tag\", \"name\": \"$repo $tag\", \"body\": \"\", \"draft\": true }" $GH_REPO/releases)
+id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
 
 # Get ID of the asset based on given filename.
-eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+#eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
 [ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
 
 # Upload asset
